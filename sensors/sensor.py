b0cbb27022b703c7be3cafc0539af48a3a9a09a0
Victor Camargo <vcamargo.e@gmail.com>
2023-02-11 12:09:56 -0500

    project config
    - Poetry added 
    - Black and flake8 formated

--- sensors/sensor.py
+++ sensors/sensor.py
@@ -1,13 +1,10 @@
 from abc import ABC, abstractmethod
 from statistics import mean
 
+
 class Sensor(ABC):
     def __init__(
-            self, 
-            id: int, 
-            threshold: float, 
-            buffer_size: int = 10, 
-            scale: float = 1
+        self, id: int, threshold: float, buffer_size: int = 10, scale: float = 1
     ):
         self.__id = id
         self.__scale = scale
@@ -15,27 +12,27 @@ class Sensor(ABC):
         self.__buffer = [0] * buffer_size
         self.__changed = True
         self.__threshold = threshold
-    
+
     @property
     def id(self) -> int:
         return self.__id
-    
+
     def read(self) -> float:
         self.__changed = False
         return self.__value
-    
+
     @property
     def has_changed(self) -> bool:
         return self.__changed
-        
+
     def sample(self):
         actual_value = self._get_value()
-        self.__buffer = [actual_value] + self.__buffer[0: -1]
+        self.__buffer = [actual_value] + self.__buffer[0:-1]
         temp_value = mean(self.__buffer)
         if abs(self.__value - temp_value) >= (self.__value * self.__threshold):
             self.__changed = True
         self.__value = temp_value
-           
+
     @abstractmethod
     def _get_value(self) -> float:
         pass
